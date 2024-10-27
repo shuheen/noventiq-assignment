0c0f3691fa0bf0555d7eedaff4eea0b80cc5627a
Shoaib <mail.shuaibkhan@gmail.com>
2024-10-27 07:34:27 +0530

    CommitLint rules addition

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,12 @@
 // commitlint.config.js
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+  },
 };
